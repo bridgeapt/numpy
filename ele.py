320e53843527d4be9dcf94d418cd403df2b7875d
bridgeapt <spoortisangur8447@gmail.com>
2025-06-21 01:21:20 +0530

    ele1

--- ele.py
+++ ele.py
@@ -1,3 +1,15 @@
 import numpy as np
-arr=np.array([10,20,40,50])
-print(arr[2])
+
+# 1D array (just a single row of numbers)
+arr1 = np.array([10, 20, 30, 40])
+print("1D Array:", arr1)
+print("First element:", arr1[0])
+print("Last element:", arr1[-1])
+
+print("\n")
+
+# 2D array (rows and columns like a table)
+arr2 = np.array([[1, 2], [3, 4]])
+print("2D Array:\n", arr2)
+print("Element at row 0, col 1:", arr2[0, 1])
+print("Second row:", arr2[1])
