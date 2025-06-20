19dad4573f39c04ac1bac48cb3fe61f0d2061d8c
bridgeapt <spoortisangur8447@gmail.com>
2025-06-21 01:10:28 +0530

    element 1

---
+++ ele.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr=np.array([10,20,40,50])
+print(arr[2])
--- nber.py
+++ nber.py
@@ -1,11 +1,8 @@
 import numpy as np
 
-arr1 = np.array([10, 20, 30]); print("Array 1:", arr1)
+arr1 = np.array(['x', 'y', 'z']); print("String Array:", arr1)
 
-arr2 = np.array([[1, 2], [3, 4]]); print("Array 2:\n", arr2)
+arr2 = np.array([[1.1, 2.2], [3.3, 4.4]]); print("Float Array:\n", arr2)
 
-arr3 = np.array([(7, 8), (9, 10)]); print("Array 3:\n", arr3)
-
-arr4 = np.array([[11, 12, 13], [14, 15, 16], [17, 18, 19]])
-print("Sliced Rows:\n", arr4[1:, :])
-print("Sliced Cols:\n", arr4[:, ::2])cl
+print("Slice row 1:", arr2[1])
+print("Slice col 0:", arr2[:, 0])
