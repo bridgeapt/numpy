6f533133f86d4103d14e327ffaefc94aaeab4f41
bridgeapt <spoortisangur8447@gmail.com>
2025-06-22 14:35:45 +0530

    adding np_03.py

---
+++ np_03.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+#join
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.concatenate((arr1, arr2))
+
+print(arr) 
+
+#join_stack 
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.stack((arr1, arr2), axis=1)
+
+print(arr) 
+
+#stack with rows and coumns
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.hstack((arr1, arr2))
+
+print(arr)
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.vstack((arr1, arr2))
+
+print(arr)
