763905579a9aef4bf9a9ef2d8106ebedd664d2ab
bridgeapt <spoortisangur8447@gmail.com>
2025-06-22 14:54:37 +0530

    adding np_04

---
+++ np_05.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#sort
+
+arr = np.array([3, 2, 0, 1])
+
+print(np.sort(arr)) 
+
+#sort 2d_array
+
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+
+print(np.sort(arr))
+
+#3d array
+
+arr = np.array([[3, 2, 4], [5, 0, 1],[4,6,5]])
+
+print(np.sort(arr))
