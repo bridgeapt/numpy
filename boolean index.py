f7918e465b08bac4f38531bba53cc20396995b43
bridgeapt <spoortisangur8447@gmail.com>
2025-06-22 14:24:49 +0530

    commit

---
+++ boolean index.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Create an array
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("All numbers:", numbers)
+
+# Show only numbers greater than 25
+print("Numbers > 25:", numbers[numbers > 25])
